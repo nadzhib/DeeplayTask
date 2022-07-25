c5c9068f99722b5313b519179cc552c473736495
nadzhib <107273103+nadzhib@users.noreply.github.com>
2022-07-25 11:03:18 +0300

    Update script.sh

--- Task1/script.sh
+++ Task1/script.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-awk '/10.1.192.38/' log.txt | grep -oP 'sid=/\K.*' | cut -d'&' -f1 | sed 's/.\{1\}$//'
+grep 10.1.192.38 log.txt | grep -oP 'sid=/\K.*' | cut -d'&' -f1 | sed 's/.\{1\}$//' | sort
